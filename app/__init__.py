e02e31b55f6cce862768d4912ee313f524149452
Frans <frans_421_12@hotmail.com>
2024-12-10 21:29:38 -0500

    feat: Config env

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+# Cargar variables del archivo .env
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
